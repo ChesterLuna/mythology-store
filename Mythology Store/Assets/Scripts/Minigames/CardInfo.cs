639b8d8978007f5c9345a1be79cd05b5f10180f9
ChesterLuna <chester99124@gmail.com>
2025-05-25 18:55:26 -0600

    Started Card scripting

---
+++ Mythology Store/Assets/Scripts/Minigames/CardInfo.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CardInfo", menuName = "Scriptable Objects/CardInfo")]
+public class CardInfo : ScriptableObject
+{
+
+    public string cardName = "";
+    public Sprite cardSprite = null;
+    
+}
---
+++ Mythology Store/Assets/Scripts/Minigames/CardManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    [SerializeField] string firstChoice = "";
+    [SerializeField] string secondChoice = "";
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
