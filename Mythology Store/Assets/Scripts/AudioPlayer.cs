273c5f8b96af929cc966c6936af385b10c7f46a0
ChesterLuna <chester99124@gmail.com>
2025-05-26 04:28:18 -0600

    Made Audio players

---
+++ Mythology Store/Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+
+    [SerializeField] AudioSource audioSource;
+
+    void Update()
+    {
+        if(audioSource.isPlaying == false)
+        {
+            Destroy(this);
+        }
+    }
+
+}
