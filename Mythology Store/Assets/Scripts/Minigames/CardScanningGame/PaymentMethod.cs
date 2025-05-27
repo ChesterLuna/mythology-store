8f6cac72d257ccaba047d9d32c784433954df03a
FlomptyCat <116393047+FlomptyCat@users.noreply.github.com>
2025-05-27 14:48:41 -0600

    Started Payment Method MiniGame
    Renamed The minigame "Card Swiping" to "Payment Method." Created and Setup the Scene with programmer art. Made some pseudo code.

---
+++ Mythology Store/Assets/Scripts/Minigames/CardScanningGame/PaymentMethod.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class PaymentMethod : MonoBehaviour
+{
+    // Bool for mouse pointer is holding card
+    // 
+
+
+    // method for Credit card
+    // method for Uno Reverse card
+    // method for Drivers License
+
+    // 
+
+    // create new classes for each triggerable object? 
+    // Ontrigger method on payment machine thingy
+    // method to reveal each card (move its position a certain y while mouse is hovering)
+    // 
+}
