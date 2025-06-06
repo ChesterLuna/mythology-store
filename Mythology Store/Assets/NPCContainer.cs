ee79bf165a0a8d60dc96f83c4fe1edab62b6d805
ChesterLuna <chester99124@gmail.com>
2025-06-06 01:27:35 -0600

    Started NPC spawner

---
+++ Mythology Store/Assets/NPCContainer.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class NPCContainer : MonoBehaviour
+{
+    public string NPCName;
+    void Awake()
+    {
+        if (NPCName == null || NPCName == "")
+        {
+            NPCName = name;
+        }
+
+        // GetComponentInChildren<MiniGameStarter>().miniGameName = NPCName;
+    }
+
+}
