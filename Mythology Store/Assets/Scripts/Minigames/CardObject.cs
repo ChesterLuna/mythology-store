e9ae422e22f407806835aaf936ae30e9a46e38a0
ChesterLuna <chester99124@gmail.com>
2025-06-07 01:20:15 -0600

    Modified logic to use different objects

--- Mythology Store/Assets/Scripts/Minigames/CardObject.cs
+++ Mythology Store/Assets/Scripts/Minigames/CardObject.cs
@@ -7,24 +7,23 @@ public class CardObject : MonoBehaviour
     [SerializeField] public CardInfo cardInfo;
     [SerializeField] public bool cardMatched = false;
 
-    private Image image;
     public CardManager cardManager;
 
-    void Start()
-    {
-        image = GetComponentInChildren<Image>();
-    }
+    [SerializeField] private Image objectImage;
+    [SerializeField] private Image backImage;
 
     public void Show()
     {
         Tween.Rotation(GetComponent<RectTransform>(), new Vector3(0f, 180f, 0f), 0.2f);
-        Tween.Delay(0.1f, () => image.sprite = cardInfo.cardSprite);
+        Tween.Delay(0.1f, () => backImage.sprite = cardInfo.frontPaperSprite);
+        Tween.Delay(0.1f, () => objectImage.sprite = cardInfo.frontSprite);
     }
 
     public void Hide()
     {
         Tween.Rotation(GetComponent<RectTransform>(), new Vector3(0f, 0f, 0f), 0.1f);
-        Tween.Delay(0.05f, () => image.sprite = cardInfo.defaultSprite);
+        Tween.Delay(0.05f, () => backImage.sprite = cardInfo.backSprite);
+        Tween.Delay(0.05f, () => objectImage.sprite = cardInfo.transparentSprite);
     }
 
     public void SetSelected()
